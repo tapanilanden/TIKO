e31f92ccf66520f47a779ef8c7d233a011b858ef
tapanilanden <Landen.Tapani.A@student.uta.fi>
2017-04-20 13:56:02 +0000

    Task & List -models + migrations

---
+++ database/migrations/2017_04_20_124821_create_list_task_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('list_task', function (Blueprint $table) {
+            $table->integer('task_id')->unsigned()->index();
+            $table->int('list_id')->unsigned()->index();
+            
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+            $table->foreign('list_id')->references('id')->on('lists')->onDelete('cascade');
+            
+            $table->unique(['task_id', 'list_id']);
+                   
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list_task');
+    }
+}
---
+++ database/migrations/2017_04_20_130703_create_lists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('kuvaus')->nullable();
+            $table->integer('user_id');
+            
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lists');
+    }
+}
