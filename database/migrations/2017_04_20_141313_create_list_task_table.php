5ac300c56c11bf12054438acda0c41a6a52841cc
tapanilanden <Landen.Tapani.A@student.uta.fi>
2017-04-20 14:14:59 +0000

    list_task migration

--- database/migrations/2017_04_20_141313_create_list_task_table.php
+++ database/migrations/2017_04_20_141313_create_list_task_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class CreateListTaskTable extends Migration
 {
     /**
@@ -24,7 +22,6 @@ class CreateListTaskTable extends Migration
                    
          });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -35,3 +32,6 @@ class CreateListTaskTable extends Migration
         Schema::dropIfExists('list_task');
     }
 }
+
+
+
