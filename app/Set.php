c565784f6f89d7414a18dd64d96571fa496393ea
zhou421513 <contact.yizhou@gmail.com>
2017-04-25 18:10:21 +0300

    Creating a set now also creates the required tables

--- app/Set.php
+++ app/Set.php
@@ -4,9 +4,31 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Facades\Schema;
+
+use Illuminate\Database\Schema\Blueprint;
+
 class Set extends Model
 {
-    
+    public function createData() {
+    	Schema::create('opiskelijat' . $this->id, function (Blueprint $table) {
+    		$table->integer('nro');
+            $table->string('nimi');
+            $table->string('p_aine');
+        });
+
+        Schema::create('kurssit' . $this->id, function (Blueprint $table) {
+    		$table->integer('id');
+            $table->string('nimi');
+            $table->string('opettaja');
+        });
+
+        Schema::create('suoritukset' . $this->id, function (Blueprint $table) {
+    		$table->integer('k_id');
+            $table->integer('op_nro');
+            $table->integer('arvosana');
+        });
+    }
 
 
 	public function answers()
