1f4f86984398abf2669a32ccc5c836ec6d3fb1ed
grebmorts <stromberg.miklos.j@student.uta.fi>
2017-04-27 16:50:43 +0300

    task lomake + layouts.errors

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -27,7 +27,7 @@ class TaskController extends Controller
      */
     public function create()
     {
-        //
+        return view('tasks.create');
     }
 
     /**
---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master')
+
+@section('content')
+
+  <div class="col-sm-8">
+
+    <h1>Luo uusi tehtävä/h1>
+
+    <hr />
+
+    <form method="POST" action={{ url('/tasks') }}>
+
+      {{ csrf_field() }}
+
+      <div class="form-group">
+        <label for="body">Tehtävän kuvaus:</label>
+        <textarea id="description" name="description" class="form-control" rows="8" required></textarea>
+      </div>
+
+      <div class="form-group">
+        <label for="body">Mallivastaus:</label>
+        <textarea id="model_query" name="model_query" class="form-control" rows="8" required></textarea>
+      </div>
+
+      <div class="col-md-6">
+          <input type="radio" name="type" value="1" checked>SELECT<br>
+          <input type="radio" name="type" value="2">INSERT<br>
+          <input type="radio" name="type" value="3">UPDATE<br>
+          <input type="radio" name="type" value="4">DELETE
+      </div>
+
+      <div class="form-group">
+      <button type="submit" class="btn btn-primary btn-block">Tallenna tehtävä</button>
+      </div>
+    </form>
+
+
+    @include ('layouts.errors')
+
+
+  </div>
+
+@endsection
