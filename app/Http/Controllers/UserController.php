3b2e8806a90e4456bf72699ed1c34ee5ae97280c
tapanilanden <Landen.Tapani.A@student.uta.fi>
2017-04-29 14:53:28 +0000

    UserController/destroy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -84,6 +84,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+        
+        return redirect()->route('users.index')->with('success', 'Käyttäjä poistettu onnistuneesti!');
     }
 }
