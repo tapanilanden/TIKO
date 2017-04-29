d16e8eaf18c7341fefe18f1e7b9d5a13b847de7f
zhou421513 <contact.yizhou@gmail.com>
2017-04-29 14:27:11 +0300

    Luo uusi tehtävä nappi

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,6 +2,10 @@
 
     @section('content')
 
+    {{ Form::open(['method' => 'get', 'action' => ['TaskController@create']]) }}
+		{{ Form::submit('Luo uusi tehtävä', ['class' => 'btn btn-default']) }}
+	{{ Form::close() }}
+
     @foreach($tasks as $task)
         <h3>{{ $task->id }} </h3>
         <p>{{ $task->body }}</p>
