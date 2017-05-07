52200e4a5d670e4a4a74de8290c56e0bbcb710fc
grebmorts <stromberg.miklos.j@student.uta.fi>
2017-05-07 14:57:57 +0300

    middlewarea lisää

--- app/Http/Middleware/CheckTask.php
+++ app/Http/Middleware/CheckTask.php
@@ -17,7 +17,7 @@ class CheckTask
      */
     public function handle($request, Closure $next)
     {
-        if ((Auth::user()->role < 3)
+        if ((Auth::user()->role == 1)
         || (Auth::user()->id == \App\Task::find($request->id)->user_id)) {
             return $next($request);
         }
